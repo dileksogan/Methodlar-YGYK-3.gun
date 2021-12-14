da8d55b1cd1a91750727b05e59930ddcc2fc37f2
Lenovo <Lenovo@LAPTOP-EO9TAI96>
2021-12-14 03:10:15 +0300

    Add project files.

---
+++ Methodlar YGYK  3.gun/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Methodlar_YGYK__3.gun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { "Elma","Karpuz" };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("---------------");
+            }
+
+            Console.WriteLine("-----------Metotlar----------");
+
+            //instance-örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+
+        }
+    }
+}
+
+
+//Dont repeat yourself - DYP - Clean Code - Best Practice
---
+++ Methodlar YGYK  3.gun/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methodlar_YGYK__3.gun
+{
+    class SepetManager
+    {
+        //naming convention
+        //normal parantez olan yerlerde method vardır.
+        //syntax(yazım şekilleri)
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(" Tebrikler. Sepete eklendi:" + urun.Adi);
+        }
+        public void ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler, sepete eklendi: " + urunAdi);
+        }
+
+
+    }
+}
